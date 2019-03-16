3dec15c9f4183f0f712a424c2c7b942eb7f09c1b
Cole <cole2247@gmail.com>
2019-03-16 00:01:43 -0400

    merge with master

--- LifeManager/app/src/main/java/pat/hacks/lifemanager/DayViewActivity.java
+++ LifeManager/app/src/main/java/pat/hacks/lifemanager/DayViewActivity.java
@@ -2,8 +2,27 @@ package pat.hacks.lifemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DayViewActivity extends AppCompatActivity {
+    TextView dateDisp = findViewById(R.id.dateTop);
+
+    dateDisp.setText("Test");
+
+
+    /*
+        int height  = Resources.getSystem().getDisplayMetrics().heightPixels;
+
+                long unixTimeSec= System.currentTimeMillis() / 1000L;
+
+                long unixTimeHour = (((unixTimeSec-(3600*4)) /3600)%24);
+
+                long unixTimeMinute = (((unixTimeSec-(3600*4))/60)%1440)%60;
+
+
+                Log.d("tag",Long.toString(unixTimeMinute));
+
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
